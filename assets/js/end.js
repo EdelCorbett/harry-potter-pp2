4db392a76d6564fde00ae7b1d4ac1bbadc3be2d5
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2023-07-17 19:39:59 +0100

    Add get highscores to end.js

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,7 +1,13 @@
 const username = document.getElementById("username");
 const saveScore = document.getElementById("saveScore");
 const finalScore = document.getElementById("finalScore");
-const mostRecentScore = localStorage.getItem("mostRecentScore");finalScore.innerText = mostRecentScore ;
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore ;
+
 
 
 username.addEventListener("keyup", () => {
@@ -11,4 +17,13 @@ username.addEventListener("keyup", () => {
 saveHighScore = e => {
     console.log("clicked the save button!");
     e.preventDefault();
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort((a,b) => b.score - a.score);
+    highScores.splice(5);
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("game.html");
 }
