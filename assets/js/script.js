734759626fbd97700f30371687609297d390c69b
EdelCorbett <131147963+EdelCorbett@users.noreply.github.com>
2023-07-23 15:44:05 +0100

    Add comments for script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const openModalBtn = document.querySelectorAll('[data-modal-target]');
 const closeModalBtn = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
 
+//event listener for open modal button
 openModalBtn.forEach(button => {
     button.addEventListener('click', () => {
         const modal = document.querySelector(button.dataset.modalTarget);
@@ -9,31 +10,34 @@ openModalBtn.forEach(button => {
     })
 })
 
-overlay.addEventListener('click', () => { 
+// event listener for overlay
+
+overlay.addEventListener('click', () => {
     const modals = document.querySelectorAll('.modal.active');
     modals.forEach(modal => {
         closeModal(modal);
     })
 })
-
-closeModalBtn.forEach(button => {   
+//event listener for close modal button
+closeModalBtn.forEach(button => {
     button.addEventListener('click', () => {
         const modal = button.closest('.modal');
         closeModal(modal);
     })
 })
 
+//function to open modal
 function openModal(modal) {
-    if(modal == null) return
+    if (modal == null) return
     modal.classList.add('active');
     overlay.classList.add('active');
 }
 
+//function to close modal
 function closeModal(modal) {
-    if(modal == null) return
+    if (modal == null) return
     modal.classList.remove('active');
     overlay.classList.remove('active');
 }
 
 console.log('Hello World from script.js');
-
